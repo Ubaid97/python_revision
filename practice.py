86bfa32f9684856189f2abb5bcee90a5f500552d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-12 10:37:29 +0000

    tuples and sets questions

--- practice.py
+++ practice.py
@@ -3,3 +3,15 @@ num_list = [1, 2, 3, 4, 5]
 print(num_list)
 num_list.append(6)
 print(num_list)
+
+# create a tuple with values 1-5
+num_tuple = (1, 2, 3, 4, 5)
+# tuples are immutable, you cannot add or remove items from them
+print(type(num_tuple))
+
+# declare a set with values 1-5, and print the values up to 3
+num_set = {1, 2, 3, 4, 5}
+for num in num_set:
+    if num <= 3:
+        print(num)
+
